94b4633ae4fda9b1504395ec60a9224675b0c482
Jack <86044816+JackZhang-nodus@users.noreply.github.com>
2024-06-24 11:40:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ echo 'load';
 	if (empty($data)) {
 		return 'Invalid signed_payload.';
 	}
-	echo 'Hello ' . json_encode($data);
+	return 'Welcome ' . json_encode($data);
 	$redis = new Credis_Client('localhost');
 	$key = getUserKey($data['store_hash'], $data['user']['email']);
 	$user = json_decode($redis->get($key), true);
